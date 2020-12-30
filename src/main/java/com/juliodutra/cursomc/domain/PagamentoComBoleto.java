e4706ac662f0e51019bbdddc47bf2453fbbe3e9d
Julio Dutra <jjcdutra@gmail.com>
2020-12-30 16:20:32 -0300

    Pedido, EstadoPagamento e Pagemento

---
+++ src/main/java/com/juliodutra/cursomc/domain/PagamentoComBoleto.java
@@ -0,0 +1,40 @@
+package com.juliodutra.cursomc.domain;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+
+import com.juliodutra.cursomc.domain.enums.EstadoPagamento;
+
+@Entity
+public class PagamentoComBoleto extends Pagamento {
+	private static final long serialVersionUID = 1L;
+	
+	private Date dataVencimento;
+	private Date dataPagemento;
+	
+	public PagamentoComBoleto() {
+	}
+
+	public PagamentoComBoleto(Integer id, EstadoPagamento estado, Pedido pedido, Date dataVencimento, Date dataPagamento) {
+		super(id, estado, pedido);
+		this.dataVencimento = dataVencimento;
+		this.dataPagemento = dataPagamento;
+	}
+
+	public Date getDataVencimento() {
+		return dataVencimento;
+	}
+
+	public void setDataVencimento(Date dataVencimento) {
+		this.dataVencimento = dataVencimento;
+	}
+
+	public Date getDataPagemento() {
+		return dataPagemento;
+	}
+
+	public void setDataPagemento(Date dataPagemento) {
+		this.dataPagemento = dataPagemento;
+	}
+}
