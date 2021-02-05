25ef0e89b847a16eafe8fdb7427540c0343eb154
Julio Dutra <jjcdutra@gmail.com>
2021-02-04 21:14:07 -0300

    Esqueci a senha

--- src/main/java/com/juliodutra/cursomc/services/AbstractMailService.java
+++ src/main/java/com/juliodutra/cursomc/services/AbstractMailService.java
@@ -13,6 +13,7 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import com.juliodutra.cursomc.domain.Cliente;
 import com.juliodutra.cursomc.domain.Pedido;
 
 public abstract class AbstractMailService implements EmailService {
@@ -70,6 +71,22 @@ public abstract class AbstractMailService implements EmailService {
 		mmh.setText(htmlFromTemplatePedido(obj), true);
 		
 		return mimeMessage;
+	}
+	
+	@Override
+	public void sendNewPasswordEmail(Cliente cliente, String newPass) {
+		SimpleMailMessage sm = prepareNewPasswordEmail(cliente, newPass);
+		sendMail(sm);
+	}
+
+	protected SimpleMailMessage prepareNewPasswordEmail(Cliente cliente, String newPass) {
+		SimpleMailMessage sm = new SimpleMailMessage();
+		sm.setTo(cliente.getEmail());
+		sm.setFrom(sender);
+		sm.setSubject("Solicitação de nova senha");
+		sm.setSentDate(new Date(System.currentTimeMillis()));
+		sm.setText("Nova senha " + newPass);
+		return sm;
 	}
 
 }
